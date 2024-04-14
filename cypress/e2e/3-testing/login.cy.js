5ecfec0ca4338eb44a80fe10f95c27a25eb77a4f
Kikkiin <125892429+Kikkiin@users.noreply.github.com>
2024-04-14 21:35:11 +0200

    Edit test files

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -4,6 +4,7 @@ describe('Login functionality', () => {
     cy.get('#loginForm input[name="email"]').type('invalidemail@example.com');
     cy.get('#loginForm input[name="password"]').type('invalidpassword');
     cy.get('#loginForm button[type="submit"]').click();
+    // cy.wait(5000);
     // cy.contains('Invalid credentials. Please try again.').should('be.visible');
   });
 });
